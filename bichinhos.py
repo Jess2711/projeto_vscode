37be2809a3fe0cff957ff81d99d669ae6f82257a
Jess2711 <jeje.rodrigues3@gmail.com>
2023-06-18 12:24:32 -0300

    commit 3 (checar temp, sobrevivencia)

--- bichinhos.py
+++ bichinhos.py
@@ -1,17 +1,21 @@
 import turtle
 import math
+from random import randint
 
-TEMP_MAX = 200
+TEMP_MAX = 200 #variaveis estaticas
 TEMP_MIN = 20
+STEP = 10
 
 class Bichinhos:
-  def __init__(self, nome, posicao, cor) -> None:
+  def __init__(self, nome, cor) -> None:
         self.nome = nome
-        self.temperatura = 60
-        self.posicao = posicao 
+        self.temperatura = 60 #não estaticas de cada objeto
+        self.posicao = __class__.gera_posicao() 
         self.caneta = turtle.Turtle()
         self.cor = cor
-        self.caneta.goto(posicao)
+        self.caneta.goto(self.posicao)
+        self.gene = __class__.gera_gene()
+
   def desenhar(self):
      self.caneta.penup()
      self.caneta.shape("turtle")
@@ -19,15 +23,63 @@ class Bichinhos:
      self.caneta.color(self.cor)
      print(self.posicao)
 
-  def mover(self):
-    self.posicao = [self.posicao[0] + 10, self.posicao[1]]
+     #valor do gene = .34/100 = 34% 
+
+  def mover(self, pos_fogueira):
+    if (randint(0,100)) <= 100*self.gene:
+       self.move_for_fogo(pos_fogueira)
+    else:
+       self.dist_to_fogo(pos_fogueira)
     self.caneta.goto(self.posicao)
+
   
+  def move_for_fogo(self, pos_fogueira):
+    aleatorio=(randint(0,1))
+    if aleatorio == 0: #mover no x
+      if self.posicao[0] < pos_fogueira[0]:
+        self.posicao[0] += STEP
+      else: 
+        self.posicao[0] -= STEP   
+    else:   
+      if self.posicao[1] < pos_fogueira[1]:
+        self.posicao[1] += STEP
+      else: 
+        self.posicao[1] -= STEP  
+
+  def dist_to_fogo(self, pos_fogueira):
+    aleatorio=(randint(0,1))
+    if aleatorio == 0: #mover no x
+      if self.posicao[0] < pos_fogueira[0]:
+        self.posicao[0] -= STEP
+      else: 
+        self.posicao[0] += STEP   
+    else:   
+      if self.posicao[1] < pos_fogueira[1]:
+        self.posicao[1] -= STEP
+      else: 
+        self.posicao[1] += STEP 
+       
     
   def calc_temp(self, pos_fogueira):
         Dist = math.sqrt((pos_fogueira[1] - self.posicao[1])**2 + (pos_fogueira[0] - self.posicao[0])**2) 
         self.temperatura = -Dist/2 + 500
-        print(pos_fogueira)
-        print(self.posicao)
-        print(Dist)
-        print(self.temperatura)
+    
+
+  def check_viver(self):
+     if self.temperatura > (TEMP_MAX) or self.temperatura < (TEMP_MIN):
+        return False
+     else:
+        return True
+
+  @staticmethod
+  def gera_posicao():
+      posicao = [(randint(-250,+250)), (randint(-250,+250))]
+      return posicao
+      
+
+  @staticmethod
+  def gera_gene():
+    gene=float(randint(0,100))/100
+    return gene
+  
+  
--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from fogueira import Fogueira
 
 cont_bichinhos = 5
 list_bichinhos = []
+list_vive = []
 fogueira = Fogueira([0,0], 90)
 
 
 
 for elemento in range(0,cont_bichinhos):
-  novo_bichinho = Bichinhos("lentao", [20,20*elemento], "red")
+  novo_bichinho = Bichinhos("lentao","red")
   list_bichinhos.append(novo_bichinho)
   
 fogueira.desenhar()
@@ -16,11 +17,21 @@ fogueira.desenhar()
 for bichinho in list_bichinhos:
   bichinho.desenhar()
   
-def rodar_simulacao():
+def rodar_simulacao(rodada):
     for bichinho in list_bichinhos:
-        bichinho.mover()
-  
-for elemento in range(0,10):
-  rodar_simulacao()
+        bichinho.mover(fogueira.posicao)
+        bichinho.calc_temp(fogueira.posicao)
+    if rodada%100 == 0:
+      listar_sobreviventes()
+        
 
-bichinho.calc_temp(fogueira.posicao)
+  
+for rodada in range(0,500):
+  rodar_simulacao(rodada)
+
+def listar_sobreviventes():
+    list_vive = []
+    for bichinho in list_bichinhos:
+      if bichinho.check_viver():
+        list_vive.append(bichinho)
+        print(bichinho.temperatura)
